9352658950ea3b9918fe59c6b08ce5dc9458920b
Derint <rebellocleophas31@gmail.com>
2023-11-17 14:14:59 +0530

    Bug Fix when ntw connection occured and added new feature to Download or Ignore a Specified file extension

--- pen.py
+++ pen.py
@@ -1,17 +1,25 @@
-from tkinter import EXCEPTION
-import urllib.parse, requests, pickle
+import urllib.parse, requests, logging
 from time import sleep, time
 import os, sys, re, argparse, json
 from bs4 import BeautifulSoup
 from datetime import datetime
-from dateutil import parser
 from math import floor, log
+from pathlib import Path
+from colorama import Back, Fore, Style
+from dateutil import parser as dateutil_parser
 
-# Crawler function
+import platform
+
+if platform.system()=='Windows':
+    from colorama import just_fix_windows_console
+    just_fix_windows_console()
+
+
+
+# Crawler Function
 def crawler(url):
     global links, folders, l, n
     animate();l+=1
-    
     req, _ = getRequest(url, verbose=False)
     soup = BeautifulSoup(req.content, features='html.parser')
 
@@ -29,26 +37,28 @@ def crawler(url):
         else:
             links.append(link)
 
+# File Saving Function
+def saveFile(url, path, chunk_size=3 * 1024 * 100, remain=[], char1="█", char2=' ', div=4): #
+    global is_conn_problem, skip, EXCEPTION_OCCURED, ONLY_ONCE, MAX_TRIES, INC_DWD_FILES
+    FALSE_COND = False, '', 0
 
-# Saving File
-def saveFile(url, path, chunk_size=3 * 1024 * 100, remain=[], char1="█", char2=' ', div=4):
-    global is_conn_problem, skip, EXCEPTION_OCCURED, ONLY_ONCE
-    req, mode = getRequest(url, 3, verbose=ONLY_ONCE)
-    if req==-1:
-        is_conn_problem=True; 
-        return False, '', 0
-    
-    ONLY_ONCE=False
+    req, mode = getRequest(url, 1, verbose=ONLY_ONCE)
+    if req == -1:
+        is_conn_problem = True
+        return FALSE_COND
+
+    ONLY_ONCE = False
     total_length = getFileSize(req)
     fn = FileName(url, only_name=True)
-    path = os.getcwd() + slash + fn if path in ['', None] else path
-    if total_length > 1e7 and chunk_size<chunk_size*10: chunk_size *= 10
+    path = CURRENT_WD + os.path.sep + fn if path in ['', None] else path
+    if total_length > 1e7 and chunk_size < chunk_size * 10:
+        chunk_size *= 10
 
-    # formated filename
+    # formatted filename
     fn = formatFileName(url)
-    tmp_size=0
-    
-    go=True
+    tmp_size = 0
+
+    go = True
     try:
         while go:
             try:
@@ -56,60 +66,51 @@ def saveFile(url, path, chunk_size=3 * 1024 * 100, remain=[], char1="█", char2
                     for chunk in req.iter_content(chunk_size=chunk_size):
                         f.write(chunk)
                         tmp_size += len(chunk)
-
                         # Progress bar
                         style_ = progressBarStyle(
-                                    fn, tmp_size, total_length, char1=char1, char2=char2, div=div, 
-                                    remain=remain, moreFiles = remain )
-                        print(style_ , end='')
+                            fn, tmp_size, total_length, char1=char1, char2=char2, div=div,
+                            remain=remain, moreFiles=remain)
+                        print(style_, end='')
+                    break
 
-                    if tmp_size>=total_length:
-                        req.close()
-                        break
+            except KeyboardInterrupt:
+                keyboardIntruption(req, path, fn)
+                skip = True
+                sleep(1.5)
+                return FALSE_COND
 
-            except (IOError):
-                backspace(5)
-                EXCEPTION_OCCURED=True
-                logIt(EXCEPTION_PATH, "File Not Found", f"url:{url}\npath:{path}")
-                sleep(1)
-                go=False
+            except (requests.exceptions.Timeout, requests.exceptions.ConnectionError, requests.exceptions.HTTPError) as e:
+                req, mode = getRequest(url, MAX_TRIES, True, tmp_size)
 
+                if req == -1:
+                    INC_DWD_FILES.append((url, path, tmp_size))
+                    os.remove(path)
+                    return FALSE_COND
             except Exception as e:
-                logIt(EXCEPTION_PATH, e)
-                e = str(e)
-                if len(e)>20:
-                    e = e[:20]
-                connectionErrorLoop(f'{e}')
-                req, mode = getRequest(url, 3, verbose=True, start_index=tmp_size)
+                logging.exception(e)
 
-                if req==-1:
-                    if os.path.exists(path):
-                        os.remove(path);
-                        is_conn_problem=True;go=False
-
-    except (ConnectionResetError):
-        connectionErrorLoop("Save file")
-        print("\r Skipping this File.... [Connection Problem]", end='')
-        sleep(5)
-        
-    except KeyboardInterrupt:
-        backspace(5)
-        req.close()
-        if os.path.exists(path):os.remove(path)
-        print(f"\r  * Skipping {fn} File....", end='')
-        skip=True; sleep(1.5)
-        return False, '', 0
-
-    except Exception as e:
-        print("EXCEPTION OCCURED: ", e)
+    except Exception as EXEC:
+        log_exeception(EXEC, True)
         sys.exit()
-    else:
-        return True, path, total_length
-    
+    return True, path, total_length
+
+def keyboardIntruption(req, path, fn):
+    backspace(5)
+    req.close()
+    if os.path.exists(path):
+        os.remove(path)
+    print(f"\r  * Skipping {fn} File....", end='')
+
+def log_exeception(exception, verbose=False):
+    logger.warning(f"EXCEPTION OCCURED:: {exception}")
+    if verbose:
+        print(f"EXCEPTION OCCURED:: {exception}")
+
 # Network/Connection functions
 def getRequest(url, max_tries=8, verbose=True, start_index=0):
     headers = {'Range': f'bytes={start_index}-'}
     encoded_url = urllib.parse.quote(url, safe=':/%=+-')
+    _maxtries = 1
     while max_tries!=-1:
         try:
             if verbose:
@@ -119,13 +120,18 @@ def getRequest(url, max_tries=8, verbose=True, start_index=0):
             return r, 'wb' if start_index==0 else 'ab'
         
         except KeyboardInterrupt:
+            backspace()
+            print("\r  ~ KeyBoard Interruption Occured ")
             sys.exit()
         except Exception as e:
             if max_tries!=0:
-                connectionErrorLoop("in Getting Request")
+                connectionErrorLoop("in Getting Request", _maxtries)
+                _maxtries+=1
             max_tries-=1    
 
-    backspace(40)
+    logger.warning("Failed to Establised connection with Host Network")
+
+    backspace(20)
     print(" \r  [!]  Connection with Host Network Failed.", end='')
     sleep(1.5)
     return -1, -1
@@ -135,7 +141,7 @@ def getLastModifiedTimeFile(path):
 
 def getLastModifedTimeURL(url):
     r, _ = getRequest(url, verbose=False)
-    t = parser.parse(r.headers['last-modified']).timestamp() if r.status_code!= 404 else None
+    t = dateutil_parser.parse(r.headers['last-modified']).timestamp() if r.status_code!= 404 else None
     return t
 
 def isModifiedFile(link, filepath):
@@ -151,10 +157,12 @@ def check_url(url):
             return r
     backspace(20)
     print(msg); 
+    logger.warning("Invalid URL or Server not active")
     exit()
 
 def getFileSize(request):
     return int(request.headers.get('content-length'))
+
 
 # String Formating
 def backspace(n=0):
@@ -194,9 +202,7 @@ def FileName(link, only_name=False):
     return getFolderName(link) + slash + file_name
 
 def getList(list_):
-    """Removes blank spaces from the lists"""
     return list(filter(lambda x: x!='', list_))
-
 
 def getDirName(url):
     if url.replace(index_link, '')=='':
@@ -209,13 +215,23 @@ def getDirName(url):
     tmp_fold = tmp_fold[:-1] if tmp_fold.endswith('/') else tmp_fold
     return tmp_fold
 
-
 def getFolders(links):
     folders = set()
     for folder in links:
         tmpFn = getFolderName(folder)
         folders.add(tmpFn+slash)
     return list(folders)
+
+def get_links_2_be_dwd(links):
+    links_2_be_downloaded = []
+    for idx, link in enumerate(links):
+        fn = FileName(link)
+        if fn.endswith(slash):continue
+        print(f'\r   *Checking for Previous/Modified File ({round(idx/len(links)*100)}%)... ', end='')
+        if not os.path.isfile(fn)  or isModifiedFile(link, fn):
+            links_2_be_downloaded.append((link, fn))
+    return links_2_be_downloaded
+
 
 # System Functions
 def createFolders(folders):
@@ -232,14 +248,14 @@ def check_empty_dir(directory):
                 list(map(lambda x: os.removedirs(x), emptyDir))
                 print("  [+]  Empty Directories Purged...")
         except:
-            print('it is what it is')
+            pass
         
         print()
 
 # Animations
 def animate():
     global l, n
-    print(f"\r\tGetting all the links " + '.' * l, end='\r')
+    print(f"\r {' '*5}Getting all the links " + '.' * l, end='\r')
     if l > n: backspace(n=50);l=1
 
 def getStyle():
@@ -281,16 +297,22 @@ def formatFileName(link, tl=25):
         ext = '.'+ext[1:][-extL+1:]
     return str(fn+ext).strip().center(tl)
 
-def connectionErrorLoop(funcName, n=8):
+def connectionErrorLoop(funcName, tryNo, n=8):
     backspace(n=5)
     for i in range(n, 0, -1):
-        print(f"\r  Connection Problem {funcName}: Reconnecting in {i} secs..", end="")
+        print(f"\r [{tryNo}/{MAX_TRIES}] Connection Problem {funcName}: Reconnecting in {i} secs..", end="")
         sleep(1);
+    backspace(10)
+
+def logAnimation():
+    print("\r Debugging Started", end='')
+    sleep(1)
+    print("\r Program Activity Will be Recorded....", end='')
+    sleep(1.5)
+    backspace()
 
 
-
-  # Calculation Functions
-
+# Calculation Functions
 def convert_size(size_bytes):
    if size_bytes == 0:
        return "0B"
@@ -319,33 +341,42 @@ def noOfFolders(path):
 
 # Reading Terminal Arguments
 def getArguments():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--url', '-u',  dest='url', help='URL of File to be downloaded.')
-    parser.add_argument('--Fold_name', '-Fn', dest='folder_name', help="Folder Name In which you want to save all the downloaded files.", default="Downloaded-Files")
-    parser.add_argument('--todirectory', '-dir', dest='dir', help="Directory Path to save all the file(s) ", default=os.getcwd())
-    parser.add_argument('-ndf', action='store_true', help="Directly Just Download the File i.e No Default Directory ")
-    return parser.parse_args()
+    argParser = argparse.ArgumentParser()
+    argParser.add_argument('--url', '-u',  dest='url', help='URL of File to be downloaded.')
+    argParser.add_argument('--Fold_name', '-Fn', dest='folder_name', help="Folder Name In which you want to save all the downloaded files.", default="Downloaded-Files")
+    argParser.add_argument('--todirectory', '-dir', dest='dir', help="Directory Path to save all the file(s) ", default=os.getcwd())
+    argParser.add_argument('-ndf', action='store_true', help="Directly Just Download the File i.e No Default Directory ")
+    argParser.add_argument('--addLogs', action='store_true', dest='addLogs', help="Add Log(s)")
+    argParser.add_argument('--maxRetries', '-xtries', dest='max_retries', help="Maximum No of Retries to Fetch the File..", default=3, type=int)
+    argParser.add_argument('--extensions', '-ext', dest='allowed_exts', help='Download only the Specified Extensions', nargs='+', default=())
+    argParser.add_argument('--ignoreExt', '-IgnExt', dest='ignore_ext', help='Ignore the Specified File Extensions', nargs='+', default=())
+    return argParser, argParser.parse_args()
 
-def logIt(_fileName, _exception, _msg=""):
-    with open(_fileName, 'a') as f:
-        f.write(f"Time:{datetime.now()}\nURL: {url}\nException: {_exception}\n{_msg}\n\n")
 
-
-args = getArguments()
-url =args.url
+argParser, args = getArguments()
+url = args.url
 m_fold_name = args.folder_name
 directory = args.dir
 ndf = args.ndf
+addLogs = args.addLogs
+MAX_TRIES = args.max_retries
+ALLOWED_EXTS = tuple(args.allowed_exts)
+IGNORE_EXTS = tuple(args.ignore_ext)
+
+if ALLOWED_EXTS and IGNORE_EXTS:
+    error_message = "Error: Only one of --extensions/-ext or --ignoreExt/IgnExt is allowed, not both."
+    print(f"  {Fore.RED}{Style.BRIGHT}{error_message}{Style.RESET_ALL}")
+    sys.exit()
 
 l, n, size, c = 1, 8, 0, 0
-links = []
+links, INC_DWD_FILES = [], []
 slash = os.path.sep
 is_conn_problem = False
 EXCEPTION_OCCURED = False
 ONLY_ONCE = True
+CURRENT_WD = os.getcwd()
 
-EXCEPTION_PATH = "Exception.txt"
-LINK_FILENAME = "tmp_link_file.tmp"
+EXCEPTION_PATH = directory + slash + "app.log"
 UNKNOWN_FILE_NAME = 'UKN-FIL-' + str(datetime.now().strftime("%d-%m-%y-%H"))
 
 m_fold_name += slash
@@ -353,17 +384,35 @@ m_fold_name += slash
 if ndf: m_fold_name = ''
     
 if directory[:2] in ['.\\', './']:
-    directory = os.getcwd() + slash + directory[2:]
+    directory = CURRENT_WD + slash + directory[2:]
     
 if not os.path.isdir(directory):
-    os.mkdir(directory)
+    os.makedirs(directory)
 
 if directory.startswith('.' +slash):
-    directory = os.getcwd() + directory.replace('.', '')
+    directory = CURRENT_WD + directory.replace('.', '')
 
 directory += slash + m_fold_name
 
-if not os.path.isdir(directory): os.mkdir(directory)
+if not os.path.isdir(directory):
+    os.mkdir(directory)
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    handlers=[logging.FileHandler(EXCEPTION_PATH)]
+)
+logger = logging.getLogger(__name__)
+if addLogs:
+    logAnimation()
+else:
+    logging.getLogger().setLevel(logging.CRITICAL + 1)
+    logging.disable(logging.NOTSET)
+    logging.shutdown()
+    os.remove(EXCEPTION_PATH)
+    
+
+logger.info("Program Started")
 
 if url is None:
     url = input("\n  [>]  Enter URL: ").strip()
@@ -380,13 +429,14 @@ with open(current_dir+slash+"ASCII-Encoding.json") as f:
 
 # Stopwatch starts now...
 s_time = time()
+
 # For Single File
 print(f"\r  *Checking if its a File ...", end='')
-if getRequest(url)[0].headers.get('last-modified'):
+if getRequest(url, 3)[0].headers.get('last-modified'):
     fname = directory+slash+ FileName(url, True)
     if not os.path.isfile(fname):
         if saveFile(url, fname)[0]:
-            backspace(n=20)
+            backspace(n=10)
             print("\r  [+]  Download Complete")
     else:
         backspace(n=20)
@@ -414,6 +464,24 @@ if not links:
     print("\r  [!]  No Files were found !!!"); 
     exit()
 
+if ALLOWED_EXTS:
+    backspace(20)
+    print("\r  *Filtering Links Based on Passed Extension....", end='')
+    links = tuple(filter(
+        lambda link: Path(link).suffix[1:] in ALLOWED_EXTS,
+        links
+    ))
+
+if IGNORE_EXTS:
+    backspace(20)
+    print("\r  *Filtering Links Based on Passed Extension....", end='')
+    links = tuple(filter(
+        lambda link: Path(link).suffix[1:] not in IGNORE_EXTS,
+        links
+    ))
+
+backspace(30)
+
 # Creating Directories
 print('\r  *Creating Directories ....', end='')
 pre_num = noOfFolders(_dir_)
@@ -421,16 +489,9 @@ folders = getFolders(links)
 createFolders(folders)
 backspace(30)
 
-
 # Checking if the file is already present in folder
-links_2_be_downloaded = []
-for idx, link in enumerate(links):
-    fn = FileName(link)
-    if fn.endswith(slash):continue
-    print(f'\r   *Checking for Previous/Modified File ({round(idx/len(links)*100)}%)... ', end='')
-    if not os.path.isfile(fn)  or isModifiedFile(link, fn) :
-        links_2_be_downloaded.append((link, fn))
 backspace(n=20)
+links_2_be_downloaded = get_links_2_be_dwd(links)
 
 if not links_2_be_downloaded:
     print(f"\r  [+]  All files Already Downloaded : @{_dir_}")
@@ -446,7 +507,6 @@ except KeyboardInterrupt:
     backspace(30)
     ok=False
 e_time = time()
-
 backspace(5)
 check_empty_dir(_dir_)
 
@@ -461,8 +521,10 @@ print("\r  [INFO]  Download Time : ", convert(round(e_time - s_time)))
 if EXCEPTION_OCCURED:
     print(f"\r  [!]  Path Not found. (See Logs @{EXCEPTION_PATH})")   
 
-
 if is_conn_problem and skip:
     print("  [!]  Some Files were not able to be Downloaded. Pls Re-run the program to download them.")
 else:
     if ok:print("\r   [+]\t  Download Complete")
+
+logger.info("Program Terminated Successfully....")
+# if not addLogs:
